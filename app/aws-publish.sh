8af49494e7eb675028859bf6ea87fe6f24c7963d
Lawrence McDaniel <lpm0073@gmail.com>
2018-03-09 14:55:16 -0600

    bug fixes

--- app/aws-publish.sh
+++ app/aws-publish.sh
@@ -4,7 +4,7 @@ export AWS_PROFILE=agility360
 
 
 echo "Building web app."
-npm run build --prod
+npm run build
 aws s3 sync ./www s3://mobile.agility360app.net --profile agility360
 echo "Web app compiled."
 echo "To execute in prod: https://mobile.agility360app.net"
